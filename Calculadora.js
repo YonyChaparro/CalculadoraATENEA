5040b5c11d0d836a4dadeda81e5304f56b122f1d
Yony <ychaparro@unal.edu.co>
2023-03-22 16:42:08 -0500

    v2.0

--- Calculadora.js
+++ Calculadora.js
@@ -4,19 +4,17 @@ function renderizarGUI() {
    const divCalculadora = document.createElement("div");
    //2.Agregar atributos
    divCalculadora.setAttribute("id", "calculadora");
-   divCalculadora.setAttribute("style", "width:50% text-aling: center");
-   divCalculadora.setAttribute("class","text-center")
-
+   divCalculadora.setAttribute("class", "text-center")
+   divCalculadora.setAttribute("style", "width: 50%; margin:0 auto");
    //3. Insertar relemento en el DOM
    document.body.appendChild(divCalculadora);
-
-
 
    //Div pantalla
    const divPantalla = document.createElement("div");
    divPantalla.setAttribute("id", "divPantalla");
-   divCalculadora.setAttribute("class", "form-control text-rigth")
-   document.body.appendChild(divPantalla);
+
+
+   divCalculadora.appendChild(divPantalla);
 
    //pantalla
    const Pantalla = document.createElement("input");
@@ -24,10 +22,14 @@ function renderizarGUI() {
    Pantalla.setAttribute("id", "Pantalla");
    Pantalla.setAttribute("type", "text");
    Pantalla.setAttribute("value", "0");
-   Pantalla.setAttribute("class", "form-control text-right")
+   Pantalla.setAttribute("class", "form-control text-right");
    Pantalla.setAttribute("disabled", "true");
-   
-   document.body.appendChild(Pantalla);
+   divPantalla.appendChild(Pantalla);
+
+   //divBotones
+   const divBotones=document.createElement("div");
+   divBotones.setAttribute("id","divBotones");
+   divCalculadora.appendChild(divBotones)
 }
 
 renderizarGUI();
