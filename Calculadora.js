576669a9bae8bc787cfa66361c8cc228175f2a98
Yony <ychaparro@unal.edu.co>
2023-03-23 15:00:24 -0500

    V3.0

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,11 @@
+/*/TODO: BOTÓN PARTA BORRAR PANTALLA
+HACER UN CATCH PARA MANEJAR LA EXCEPCIÓN DE LA EXPRECIÓN NO VÁLIDA
+*/
 
 function renderizarGUI() {
+
+   var Botones=["7","8","9","*","4","5","6","+","1","2","3","-","0","/",".","="]
+
    // 1. Crear elemento
    const divCalculadora = document.createElement("div");
    //2.Agregar atributos
@@ -30,6 +36,47 @@ function renderizarGUI() {
    const divBotones=document.createElement("div");
    divBotones.setAttribute("id","divBotones");
    divCalculadora.appendChild(divBotones)
+
+   for (let i = 0; i < Botones.length; i++) {
+
+      //Creamos las filas
+      if (i%4==0) {
+         const divfila=document.createElement("div")
+         divfila.setAttribute("class", "row")
+         divBotones.appendChild(divfila)
+      }
+
+      let Boton=document.createElement("button");
+      Boton.setAttribute("id","boton"+Botones[i]);
+      Boton.innerHTML=Botones[i]
+      Boton.setAttribute("class", "btn")
+      Boton.setAttribute("class", "btn btn-light col-3 border-white")
+
+      divBotones.appendChild(Boton)
+      divCalculadora.appendChild(Boton);
+
+      //Agregar un escuchador de eventos 
+      Boton.addEventListener("click", function() {  
+         procesarEvento(Boton);
+      });
+
+      divBotones.lastChild.appendChild(Boton)
+
+   }
+}
+
+function procesarEvento(Boton) {
+   let miPantalla=document.getElementById("Pantalla");
+   if (miPantalla.value=="0") {
+      miPantalla.value="";
+   }
+   if (Boton.innerHTML != "=") {
+      miPantalla.value+=Boton.innerHTML;
+   }else{
+      //Evaluar la expreción con Math.js
+      let resultado=math.evaluate(miPantalla.value);
+      miPantalla.value=resultado;
+   }
 }
 
 renderizarGUI();
